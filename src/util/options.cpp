485941972a29e823edc54f04fb8899c50685b477
Sam Nesbitt <sam@snesb.com>
2024-09-23 18:33:25 +1000

    Move options to tsor namespace

--- src/util/options.cpp
+++ src/util/options.cpp
@@ -4,69 +4,71 @@
 
 #include "options.h"
 
-cxxopts::ParseResult parse_args(int argc, char* argv[]) {
-    try {
-        cxxopts::Options options(argv[0]);
-        options.add_options()
-            ("i,input", "Input file", cxxopts::value<std::string>())
-            ("filter", "List of PIDs to process (hex)", cxxopts::value<std::string>())
-            ("v,verbose", "Enable verbose logging", cxxopts::value<bool>()->default_value("false"))
-            ("h,help", "Print usage")
-        ;
-        options.parse_positional({"input"});
-        cxxopts::ParseResult args = options.parse(argc, argv);
+namespace tsor {
+    cxxopts::ParseResult parse_args(int argc, char* argv[]) {
+        try {
+            cxxopts::Options options(argv[0]);
+            options.add_options()
+                ("i,input", "Input file", cxxopts::value<std::string>())
+                ("filter", "List of PIDs to process (hex)", cxxopts::value<std::string>())
+                ("v,verbose", "Enable verbose logging", cxxopts::value<bool>()->default_value("false"))
+                ("h,help", "Print usage")
+            ;
+            options.parse_positional({"input"});
+            cxxopts::ParseResult args = options.parse(argc, argv);
 
-        // Print usage info if no args supplied or "input" arg is missing
-        if (argc == 1 || args.count("help") || !args.count("input")) {
-            std::cout << options.positional_help("<input>").help() << std::endl;
+            // Print usage info if no args supplied or "input" arg is missing
+            if (argc == 1 || args.count("help") || !args.count("input")) {
+                std::cout << options.positional_help("<input>").help() << std::endl;
+                exit(1);
+            }
+
+            return args;
+        }
+        catch (const cxxopts::exceptions::exception& e) {
+            std::cout << "Error parsing options: " << e.what() << std::endl;
             exit(1);
         }
-
-        return args;
     }
-    catch (const cxxopts::exceptions::exception& e) {
-        std::cout << "Error parsing options: " << e.what() << std::endl;
-        exit(1);
-    }
-}
 
-std::vector<uint16_t> parse_filter(const cxxopts::OptionValue* filter, bool verbose) {
-    auto list = filter->as<std::string>();
-    std::vector<uint16_t> pids = {};
-    size_t start = 0, end;
-    std::string token;
+    std::vector<uint16_t> parse_filter(const cxxopts::OptionValue* filter, bool verbose) {
+        auto list = filter->as<std::string>();
+        std::vector<uint16_t> pids = {};
+        size_t start = 0, end;
+        std::string token;
 
-    if (list.length() == 0) return pids;
- 
-    while ((end = list.find(",", start)) != std::string::npos) {
-        // Search for comma delimiter
-        token = list.substr(start, end - start);
-        start = end + 1;
+        if (list.length() == 0) return pids;
+    
+        while ((end = list.find(",", start)) != std::string::npos) {
+            // Search for comma delimiter
+            token = list.substr(start, end - start);
+            start = end + 1;
 
-        // Convert hex string to int and add to vector
+            // Convert hex string to int and add to vector
+            if (!add_filter_pid(&pids, token) && verbose) std::cerr << "Filter PID \"" << token << "\" is invalid" << std::endl;
+        }
+
+        // Handle last element in list
+        token = list.substr(start);
         if (!add_filter_pid(&pids, token) && verbose) std::cerr << "Filter PID \"" << token << "\" is invalid" << std::endl;
+
+        return pids;
     }
 
-    // Handle last element in list
-    token = list.substr(start);
-    if (!add_filter_pid(&pids, token) && verbose) std::cerr << "Filter PID \"" << token << "\" is invalid" << std::endl;
+    bool add_filter_pid(std::vector<uint16_t>* v, std::string s) {
+        // Basic input validation
+        if (s.length() == 0 || s.find(" ", 0) != std::string::npos) return false;
 
-    return pids;
-}
+        try {
+            // Convert hex string to int
+            uint16_t pid = std::stoi(s, 0, 16);
 
-bool add_filter_pid(std::vector<uint16_t>* v, std::string s) {
-    // Basic input validation
-    if (s.length() == 0 || s.find(" ", 0) != std::string::npos) return false;
+            // Maximum possible PID value (13 bits)
+            if (pid > 0x1FFF) throw std::out_of_range("Must be less than 0x1FFF");
+            v->push_back(pid);
+        }
+        catch (...) { return false; }   // std::out_of_range, std::invalid_argument
 
-    try {
-        // Convert hex string to int
-        uint16_t pid = std::stoi(s, 0, 16);
-
-        // Maximum possible PID value (13 bits)
-        if (pid > 0x1FFF) throw std::out_of_range("Must be less than 0x1FFF");
-        v->push_back(pid);
+        return true;
     }
-    catch (...) { return false; }   // std::out_of_range, std::invalid_argument
-
-    return true;
-}
+} // namespace tsor
